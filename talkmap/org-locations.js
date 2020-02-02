78df689edd8e9d04f0b96b179e743bf48432dc92
Stephen Doyle <sd21@sanger.ac.uk>
2020-02-02 22:05:20 +0000

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -9,10 +9,62 @@ var addressPoints = [
   ],
   [
     "Natural History Museum, London, UK",
-    51.6884325,-1.2968817
+    51.496715,-0.1785559
   ],
   [
     "Royal Veterinary College, London, UK",
     51.5366976,-0.1361583
+  ],
+  [
+    "Imperial College London, London, UK",
+    51.4987997,-0.1770659
+  ],
+  [
+    "La Trobe University, Melbourne, Australia",
+    -37.7206671,145.0462253
+  ],
+  [
+    "Institute of Parasitology, McGill University, Canada",
+    45.4061022,-73.9437682
+  ],
+  [
+    "WAAVP, Madison, USA",
+    43.0746981,-89.3864436
+  ],
+  [
+    "CARS, Madison, USA",
+    43.0718524,-89.4079886
+  ],
+  [
+    "C.elegans and otehr nematodes, Wellcome Sanger Institute, UK",
+    52.0797096,0.1835767
+  ],
+  [
+    "Earlham Insitute, Norwich, UK",
+    52.6217407,1.2167982
+  ],
+  [
+    "BSP, Dundee, UK",
+    56.4559831,-2.9897541
+  ],
+  [
+    "INRA, Tours, France",
+    47.3942462,0.6249003
+  ],
+  [
+    "ASP, Canberra, Australia",
+    -35.2792198,149.1155737
+  ],
+  [
+    "Anthelmintics meeting, San Fransisco, USA",
+    37.8066563,-122.4314998
+  ],
+  [
+    "Molecular Helminthology, Hydra, Greece",
+    37.3482373,23.4625237
+  ],
+  [
+    "ASP, Launceston, Australia",
+    -41.4347244,147.1014439
   ]
 ];
